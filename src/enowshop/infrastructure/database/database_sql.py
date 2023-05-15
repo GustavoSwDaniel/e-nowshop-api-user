5cf11b0dd5fae3e5086400bead2f85ae019df4ee
GustavoSwDaniel <gustavodanieldetoledo@gmail.com>
2023-05-14 21:46:36 -0300

    fix: adjuts deployemts file in pipeline

--- src/enowshop/infrastructure/database/database_sql.py
+++ src/enowshop/infrastructure/database/database_sql.py
@@ -13,7 +13,6 @@ Base = declarative_base()
 
 class PostgresDatabase:
     def __init__(self, database_url: str) -> None:
-        print(database_url)
         self.__engine = create_async_engine(database_url, echo=False, pool_size=5, max_overflow=10)
         self.__session_factory = async_scoped_session(orm.sessionmaker(self.__engine, expire_on_commit=False,
                                                                        class_=AsyncSession), scopefunc=current_task)
@@ -24,6 +23,7 @@ class PostgresDatabase:
     @asynccontextmanager
     async def session(self) -> AsyncSession:
         session: AsyncSession = self.__session_factory()
+        print('TO AQUI DENTRO O CARALHO DE ASA')
         try:
             yield session
         except Exception:
